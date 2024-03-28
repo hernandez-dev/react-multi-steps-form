992d19cda2db0e3cccfc0351fc702adb86a8910f
Hernandez Maikol <hernandezdev@proton.me>
2024-03-28 16:15:51 -0400

    Project - App - creating a basic layout representation ( box-model )

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+// components
+import Sidebar from "./components/sidebar/Sidebar.jsx"
+import StepsForm from "./components/steps-form/StepsForm.jsx"
+
 function App() {
   return (
-    <div className="text-center text-red-500">
-      react + tailwindCss
-    </div>
+    <main className="flex space-x-1 m-auto w-full h-full p-1 bg-white rounded font-bold text-sky-600">
+      <Sidebar />
+      <StepsForm />
+    </main>
   )
 }
 
