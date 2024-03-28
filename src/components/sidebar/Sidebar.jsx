aa986b2c4b99bb5490b60041dbb3b073c4d0382e
Hernandez Maikol <hernandezdev@proton.me>
2024-03-28 16:14:56 -0400

    Project - components - building components basic layout ( Sidebar and StepsForm )

---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+export default function Sidebar() {
+  return(
+    <aside className="flex items-center justify-center bg-sky-100 w-[250px] p-2 rounded">
+      Sidebar
+    </aside>
+  )
+}
---
+++ src/components/steps-form/StepsForm.jsx
@@ -0,0 +1,12 @@
+export default function StepsForm() {
+  return(
+    <form className="flex flex-col flex-1 space-y-1">
+      <section className="flex items-center justify-center flex-1 p-2 bg-sky-100 rounded capitalize">
+        form steps
+      </section>
+      <footer className="flex items-center justify-center h-14 p-2 rounded bg-sky-100 capitalize">
+        form buttons
+      </footer>
+    </form>
+  )
+}
