7016c10cb6ea8a6b6a5e4df9828b28f5780c7b64
Hernandez Maikol <hernandezdev@proton.me>
2024-03-29 16:28:21 -0400

    Project - reducer.js - creating file basic reducer cases

---
+++ src/reducer.js
@@ -0,0 +1,12 @@
+const reducer = (draft, action) => {
+  switch(action.type) {
+    case "set-name":
+      break
+    case "set-email":
+      break
+    case "set-phone-number":
+      break
+  } // switch end
+}
+
+export default reducer
