f083ce7312c79958932740ec17d5b1d40a4d6bb4
Hernandez Maikol <hernandezdev@proton.me>
2024-03-29 16:28:54 -0400

    Project - App.jsx - settging up application state use useImmerReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
-// app state
-import state from "./state.jsx"
+import { useImmerReducer } from "use-immer"
+
+// app initial state
+import { state as initialState } from "./state.jsx"
+
+// reducer
+import reducer from "./reducer.js"
 
 // context
 import StateContext from "./context/StateContext.js"
@@ -10,9 +15,12 @@ import Sidebar from "./components/sidebar/Sidebar.jsx"
 import StepsForm from "./components/steps-form/StepsForm.jsx"
 
 function App() {
+  // state
+  const [state, dispatch] = useImmerReducer(reducer, initialState)
+
   return (
     <StateContext.Provider value={state}>
-      <DispatchContext.Provider value={1}>
+      <DispatchContext.Provider value={dispatch}>
         <main className="flex space-x-1 m-auto w-full max-w-6xl p-4 bg-white rounded-xl shadow-lg">
           <Sidebar steps={state.steps} />
           <StepsForm steps={state.steps} />
