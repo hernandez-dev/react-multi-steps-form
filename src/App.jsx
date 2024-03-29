ea1058fbcf43a77e855225e10accc152f700166c
Hernandez Maikol <hernandezdev@proton.me>
2024-03-29 16:05:37 -0400

    Project - App.jsx - wrapping our app into context providers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
+// app state
+import state from "./state.jsx"
+
+// context
+import StateContext from "./context/StateContext.js"
+import DispatchContext from "./context/DispatchContext.js"
+
 // components
 import Sidebar from "./components/sidebar/Sidebar.jsx"
 import StepsForm from "./components/steps-form/StepsForm.jsx"
 
 function App() {
   return (
-    <main className="flex space-x-1 m-auto w-full h-full p-1 bg-white rounded font-bold text-sky-600">
-      <Sidebar />
-      <StepsForm />
-    </main>
+    <StateContext.Provider value={state}>
+      <DispatchContext.Provider value={1}>
+        <main className="flex space-x-1 m-auto w-full max-w-6xl p-4 bg-white rounded-xl shadow-lg">
+          <Sidebar steps={state.steps} />
+          <StepsForm steps={state.steps} />
+        </main>
+      </DispatchContext.Provider>
+    </StateContext.Provider>
   )
 }
 
