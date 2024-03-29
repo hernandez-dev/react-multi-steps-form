926e2fc0e143cb8ac66ef0b77af2e08b65b549d2
Hernandez Maikol <hernandezdev@proton.me>
2024-03-29 17:02:43 -0400

    Components - steps-form/components - applying theme colors to FormControl

--- src/components/steps-form/components/FormControl.jsx
+++ src/components/steps-form/components/FormControl.jsx
@@ -1,16 +1,16 @@
 export default function FormControl({ element, dispatch }) {
   return(
     <div className="space-y-3 mt-5 first:mt-0">
-      <label htmlFor={element.id} className="block w-full leading-none">
+      <label htmlFor={element.id} className="block w-full text-marine-blue leading-none">
         {element.label}
       </label>
-      <div className="border border-gray-300 rounded-md overflow-hidden">
+      <div className="border border-light-gray rounded-lg overflow-hidden">
         <input
           id={element.id}
           type={element.type}
           name={element.name}
           value={element.value}
-          className="outline-none block w-full px-2 background-transparent leading-10"
+          className="outline-none block w-full px-4 background-transparent text-cool-gray leading-[3rem]"
           onChange={(e) => dispatch({ type: "", value: e.target.value })}
         />
       </div>
