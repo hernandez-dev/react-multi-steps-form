146ae2870dc52f8bedd7d5562984d1ca23254618
Hernandez Maikol <hernandezdev@proton.me>
2024-03-29 16:58:11 -0400

    Components - steps-form/components - applying theme colors to FormHeader

--- src/components/steps-form/components/FormHeader.jsx
+++ src/components/steps-form/components/FormHeader.jsx
@@ -1,10 +1,10 @@
 export default function FormHeader({ step }) {
   return(
     <header className="space-y-4">
-      <h1 className="font-bold text-3xl leading-none">
+      <h1 className="font-bold text-3xl text-marine-blue leading-none">
         {step.heading}
       </h1>
-      <p className="leading-none">
+      <p className="text-cool-gray leading-none">
         {step.description}
       </p>
     </header>
