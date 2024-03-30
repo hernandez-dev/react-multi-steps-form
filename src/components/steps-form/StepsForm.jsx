34a7af826c0433ae2e479de108131da439ed2e21
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 12:49:54 -0400

    Components - steps-form - rendering form buttons

--- src/components/steps-form/StepsForm.jsx
+++ src/components/steps-form/StepsForm.jsx
@@ -3,17 +3,42 @@ import { useContext } from "react"
 // context
 import StateContext from "../../context/StateContext.js"
 
+// components
+import Button from "./components/Button.jsx"
+
 export default function StepsForm() {
   // app state
   const { steps, currentStep } = useContext(StateContext)
 
+  // current step
+  const step = steps[currentStep]
+
   return(
     <form className="flex flex-col flex-1 space-y-1">
-      <section className="flex items-center justify-center flex-1 p-2 bg-sky-100 rounded capitalize">
-        form steps
+      <section className="flex-1">
+        current step
       </section>
-      <footer className="flex items-center justify-center h-14 p-2 rounded bg-sky-100 capitalize">
-        form buttons
+      <footer className="flex justify-between">
+        <Button
+          type="button"
+          action="prev"
+          bg="bg-transparent"
+          color="text-cool-gray"
+          disabled={currentStep === 0}
+          onClick={() => console.log("click")}
+        >
+          go back
+        </Button>
+        <Button
+          type="button"
+          action="prev"
+          bg="bg-marine-blue"
+          color="text-white"
+          disabled={currentStep === (steps.length - 1)}
+          onClick={() => console.log("click")}
+        >
+          next step
+        </Button>
       </footer>
     </form>
   )
