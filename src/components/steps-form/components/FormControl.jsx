7cdf5d4841474d159d5549663ce270404aa67124
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 12:35:05 -0400

    Components - steps-form/components - renaming destructured prop at FormControl ( element -> field )

--- src/components/steps-form/components/FormControl.jsx
+++ src/components/steps-form/components/FormControl.jsx
@@ -1,17 +1,22 @@
-export default function FormControl({ element, dispatch }) {
+export default function FormControl({ field, index, dispatch }) {
   return(
     <div className="space-y-3 mt-5 first:mt-0">
-      <label htmlFor={element.id} className="block w-full text-marine-blue leading-none">
-        {element.label}
+      <label htmlFor={field.id} className="block w-full text-marine-blue leading-none">
+        {field.label}
       </label>
       <div className="border border-light-gray rounded-lg overflow-hidden">
         <input
-          id={element.id}
-          type={element.type}
-          name={element.name}
-          value={element.value}
+          id={field.id}
+          type={field.type}
+          name={field.name}
+          value={field.value}
           className="outline-none block w-full px-4 background-transparent text-cool-gray leading-[3rem]"
-          onChange={(e) => dispatch({ type: "", value: e.target.value })}
+          onChange={e => dispatch({
+            type: "set-form-control",
+            step: 0,
+            index: index,
+            value: e.target.value
+          })}
         />
       </div>
     </div>
