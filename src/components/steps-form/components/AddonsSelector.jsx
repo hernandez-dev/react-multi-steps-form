c03d62f8dffb895a672f5e489a707b86b9b23fe2
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 17:12:08 -0400

    Components - steps-form/components - dispatching new event at PlanSelector ( select-plan )

--- src/components/steps-form/components/AddonsSelector.jsx
+++ src/components/steps-form/components/AddonsSelector.jsx
@@ -9,14 +9,20 @@ export default function AddonsSelector({
 
   return(
     <div className="space-y-4">
-      {step.addons.map(addon => {
+      {step.addons.map((addon, index) => {
         return(
           <button
             key={addon.id}
             type="button"
-            className={`flex items-center w-full p-5 border ${addon.selected ? 'bg-sky-100/30 border-sky-400' : 'border-gray-300'} rounded-lg`}
+            className={`flex items-center w-full p-5 border ${addon.selected ? 'bg-sky-100/30 border-sky-400' : 'border-gray-300'} rounded-lg transition duration-300`}
+            onClick={() => dispatch({
+              type: "select-addon",
+              step: 2,
+              index: index,
+              value: !addon.selected
+            })}
           >
-            <span className={`flex items-center justify-center w-6 h-6 p-1 border rounded ${addon.selected ? 'bg-sky-300 border-sky-300' : 'border-gray-300'}`}>
+            <span className={`flex items-center justify-center w-6 h-6 p-1 border rounded ${addon.selected ? 'bg-sky-300 border-sky-300' : 'border-gray-300'} transition duration-300`}>
               <img src={`/images/icon-checkmark.svg`} alt="icon checkmark" className="block w-full" />
             </span>
             <div className="flex-1 pl-5 space-y-2 text-left">
