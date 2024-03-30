4f7f1bbc72f8613a7d1dc75ff1e41926db855be1
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 12:37:49 -0400

    Components - steps-form/components - receicing new props at Button component

--- src/components/steps-form/components/Button.jsx
+++ src/components/steps-form/components/Button.jsx
@@ -1,10 +1,20 @@
 export default function Button({
+  type,
+  action,
   bg,
   color,
+  disabled,
+  onClick,
   children
 }) {
   return(
-    <button className={`block w-32 ${bg} rounded-lg font-semibold text-lg ${color} capitalize leading-[3rem]`}>
+    <button
+      type={type}
+      data-action={action}
+      className={`block w-32 ${bg} rounded-lg font-semibold text-lg ${color} capitalize leading-[3rem]`}
+      disabled={disabled}
+      onClick={onClick}
+    >
       {children}
     </button>
   )
