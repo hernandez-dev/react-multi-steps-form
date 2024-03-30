75e2b6e7d12652ec8f20d1dd7d12af137626b27d
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 12:38:37 -0400

    Components - steps-form/components - renaming FormControl prop ( element -> field )

--- src/components/steps-form/components/PersonalInformation.jsx
+++ src/components/steps-form/components/PersonalInformation.jsx
@@ -4,7 +4,7 @@ import FormControl from "./FormControl.jsx"
 export default function PersonalInformation({ step, dispatch }) {
   return(
     <div className="space-y-4">
-      {step.fields.map(field => <FormControl key={field.id} element={field} dispatch={dispatch} />)}
+      {step.fields.map((field, index) => <FormControl key={field.id} field={field} index={index} dispatch={dispatch} />)}
     </div>
   )
 }
