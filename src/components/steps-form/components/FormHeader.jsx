6c7235a7ad6a0b0c2c985d34f5f9b2d6e075d898
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 16:31:56 -0400

    Components - steps-form/components - capitalizing texts first letter at FormHeader

--- src/components/steps-form/components/FormHeader.jsx
+++ src/components/steps-form/components/FormHeader.jsx
@@ -2,10 +2,10 @@ export default function FormHeader({ step }) {
   return(
     <header className="space-y-4">
       <h1 className="font-bold text-3xl text-marine-blue leading-none">
-        {step.heading}
+        {step.heading.charAt(0).toUpperCase() + step.heading.slice(1)}
       </h1>
       <p className="text-cool-gray leading-none">
-        {step.description}
+        {step.description.charAt(0).toUpperCase() + step.description.slice(1)}
       </p>
     </header>
   )
