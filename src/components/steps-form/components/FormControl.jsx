d1868eeb742fa0307cd395b0faa887801d56d6a4
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 16:36:22 -0400

    Components - steps-form/components - moving border styles from div to input at FormControl

--- src/components/steps-form/components/FormControl.jsx
+++ src/components/steps-form/components/FormControl.jsx
@@ -4,13 +4,13 @@ export default function FormControl({ field, index, dispatch }) {
       <label htmlFor={field.id} className="block w-full font-semibold text-marine-blue capitalize leading-none">
         {field.label}
       </label>
-      <div className="border border-light-gray rounded-lg overflow-hidden">
+      <div className="overflow-hidden">
         <input
           id={field.id}
           type={field.type}
           name={field.name}
           value={field.value}
-          className="outline-none block w-full px-4 background-transparent text-cool-gray leading-[3rem]"
+          className="outline-none block w-full px-4 background-transparent border border-light-gray rounded-lg text-cool-gray leading-[3rem]"
           onChange={e => dispatch({
             type: "set-form-control",
             step: 0,
