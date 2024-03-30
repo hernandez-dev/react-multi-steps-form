d314db2bc81b73e239b0c35aab18fc59ef4f4701
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 15:37:34 -0400

    Components - sidebar - adding visual hint to current step

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -5,16 +5,17 @@ import StateContext from "../../context/StateContext.js"
 
 export default function Sidebar() {
   // state
-  const { steps } = useContext(StateContext)
+  const { steps, currentStep } = useContext(StateContext)
 
   return(
     <aside className="relative">
       <img src="/images/bg-sidebar-desktop.svg" alt="sidebar background" className="block w-full" />
-      <div className="absolute top-0 left0-0 z-10 w-full h-full p-7 space-y-7">
+      <div className="absolute top-0 left-0 z-10 w-full h-full p-7 space-y-7">
         {steps.map((step, index) => {
+          const active = index === currentStep
           return(
             <div key={step.id} className="flex items-center">
-              <span className="flex items-center justify-center w-9 h-9 border border-white rounded-full text-white">
+              <span className={`flex items-center justify-center w-9 h-9 border ${active ? "bg-pastel-blue border-patel-blue text-marine-blue" : "border-white text-white"} font-bold rounded-full`}>
                 {index + 1}
               </span>
               <div className="flex-1 pl-5 text-white uppercase tracking-widest">
