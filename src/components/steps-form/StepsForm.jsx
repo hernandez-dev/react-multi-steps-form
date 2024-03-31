9738ab8a9aa78c3b5540f1703c1f6bee34c155bf
Hernandez Maikol <hernandezdev@proton.me>
2024-03-31 17:09:13 -0400

    Components - steps-form - setting min height to main section ( steps container )

--- src/components/steps-form/StepsForm.jsx
+++ src/components/steps-form/StepsForm.jsx
@@ -30,12 +30,12 @@ export default function StepsForm() {
 
   return(
     <div className="desktop:flex-1">
-      <form className="flex flex-col flex-1 h-full desktop:max-w-2xl desktop:mx-auto">
-        <section className="relative space-y-10 p-5 m-4 -mt-[105px] bg-white rounded-lg shadow-lg desktop:flex-1 desktop:mx-0 desktop:-mt-0 desktop:shadow-none">
+      <form className="flex flex-col min-h-[485px] flex-1 desktop:max-w-2xl desktop:h-full desktop:mx-auto desktop:min-h-full">
+        <section className="relative space-y-10 p-5 m-4 -mt-[105px] bg-white rounded-lg shadow-lg desktop:mx-0 desktop:-mt-0 desktop:shadow-none">
           <FormHeader step={step} />
           <step.Component step={step} dispatch={appDispatch} />
         </section>
-        <footer className="flex justify-between bg-white p-4 rounded-lg desktop:bg-transparent desktop:p-0">
+        <footer className="flex justify-between mt-auto bg-white p-4 rounded-lg desktop:bg-transparent desktop:p-0 desktop:mt-none">
           <Button
             type="button"
             action="prev"
