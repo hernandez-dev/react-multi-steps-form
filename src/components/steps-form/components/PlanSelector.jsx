56a9bed09618b9d1d28abcd6b8fd7dadfac39926
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 20:53:40 -0400

    Components - steps-form/components - applying theme colors to PlanSelector elements

--- src/components/steps-form/components/PlanSelector.jsx
+++ src/components/steps-form/components/PlanSelector.jsx
@@ -7,20 +7,20 @@ export default function PlanSelector({ step, dispatch }) {
             <button
               key={modality.id}
               type="button"
-              className={`flex flex-col justify-between h-[250px] p-4 border ${modality.selected ? 'bg-sky-100/30 border-sky-500' : 'border-gray-300'} rounded-lg text-left transition duration-300`}
+              className={`outline-none flex flex-col justify-between h-[250px] p-4 border ${modality.selected ? 'bg-purplish-blue/5 border-purplish-blue' : 'border-gray-300'} rounded-lg text-left transition duration-300`}
               onClick={() => dispatch({ type: "select-plan", step: 1, id: modality.id })}
             >
               <span className="block w-12">
                 <img src={`/images/${modality.icon}`} alt={`${modality.name} icon`} className="block w-full" />
               </span>
               <div className="space-y-3">
-                <h4 className="font-bold text-lg capitalize leading-none">
+                <h4 className="font-bold text-lg capitalize text-marine-blue leading-none">
                   {modality.name}
                 </h4>
-                <p className="leading-none">
+                <p className="text-cool-gray leading-none">
                   ${modality.price}/{step.monthly ? 'mo' : 'yr'}
                 </p>
-                <span className="block leading-none">
+                <span className="block text-marine-blue leading-none">
                   2 months free
                 </span>
               </div>
@@ -28,18 +28,18 @@ export default function PlanSelector({ step, dispatch }) {
           )
         })}
       </div>
-      <div className="flex items-center justify-center h-12 bg-gray-100 rounded-lg space-x-3 font-bold tracking-wide">
-        <span className="block leading-none">
+      <div className="flex items-center justify-center h-12 bg-purplish-blue/5 rounded-lg space-x-6 font-bold capitalize tracking-wide">
+        <span className="block text-marine-blue leading-none">
           monthly
         </span>
         <button
           type="button"
-          className={`flex ${step.monthly ? '' : 'justify-end'} w-12 p-1 bg-sky-500 rounded-full`}
+          className={`flex ${step.monthly ? '' : 'justify-end'} w-12 p-1 bg-marine-blue rounded-full`}
           onClick={() => dispatch({ type: "select-monthly-plan", step: 1, value: !step.monthly })}
         >
           <span className="block w-4 h-4 rounded-full bg-white"></span>
         </button>
-        <span className="block leading-none">
+        <span className="block text-marine-blue leading-none">
           yearly
         </span>
       </div>
