bd661891f1d5cc0dbcc16a31195563d7d50caa8c
Hernandez Maikol <hernandezdev@proton.me>
2024-03-30 20:42:07 -0400

    Project - reducer - crearing summary object on current step changes

--- src/reducer.js
+++ src/reducer.js
@@ -3,6 +3,15 @@ const reducer = (draft, action) => {
   switch(action.type) {
     case "set-current-step":
       draft.currentStep = action.value
+      const plan = draft.steps[1].modalities.find(prev => prev.selected)
+      plan.monthly = draft.steps[1].monthly
+      const multiplier = plan.monthly ? 1 : 10
+      const summary = {
+        plan: { ...plan, price: plan.price * multiplier },
+        addons: draft.steps[2].addons.filter(prev => prev.selected).map(addon => ({ ...addon, price: addon.price * multiplier })),
+        label: plan.monthly ? "mo" : "yr"
+      }
+      draft.steps[3].summary = summary
       break
     case "set-form-control":
       draft.steps[action.step].fields[action.index].value = action.value
