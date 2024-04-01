1fc89a6a2a583a8aecf66eff49876a0a5cbf6875
Hernandez Maikol <hernandezdev@proton.me>
2024-04-01 10:17:19 -0400

    Components - steps-form/components - passing new props to Button ( hover & focus )

--- src/components/steps-form/components/Button.jsx
+++ src/components/steps-form/components/Button.jsx
@@ -5,13 +5,15 @@ export default function Button({
   color,
   disabled,
   onClick,
+  hover,
+  focus,
   children
 }) {
   return(
     <button
       type={type}
       data-action={action}
-      className={`block w-32 ${bg} rounded-lg font-semibold text-lg ${color} capitalize leading-[3rem]`}
+      className={`block w-32 ${bg} rounded-lg font-semibold text-lg ${color} capitalize leading-[3rem] transition duration-100 ${hover} ${focus}`}
       disabled={disabled}
       onClick={onClick}
     >
