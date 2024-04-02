e46f3488a36b1762c5dbaa8b2d343ffd908f15f4
Hernandez Maikol <hernandezdev@proton.me>
2024-04-02 14:46:41 -0400

    Components - steps-form/components - passing new props to FromControl ( invalid, hint ... )

--- src/components/steps-form/components/PersonalInformation.jsx
+++ src/components/steps-form/components/PersonalInformation.jsx
@@ -4,7 +4,15 @@ import FormControl from "./FormControl.jsx"
 export default function PersonalInformation({ step, dispatch }) {
   return(
     <div className="space-y-4">
-      {step.fields.map((field, index) => <FormControl key={field.id} field={field} index={index} dispatch={dispatch} />)}
+      {step.fields.map((field, index) => (
+        <FormControl
+          key={field.id}
+          field={field}
+          index={index}
+          invalid={field.invalid}
+          hint={field.hint}
+          dispatch={dispatch} />
+      ))}
     </div>
   )
 }
