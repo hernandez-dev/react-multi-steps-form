145851b1b2fb9f25db3ed80af60401e9917410d8
Hernandez Maikol <hernandezdev@proton.me>
2024-04-02 15:19:20 -0400

    Components - steps-form - importing and rendering Confirmation component

--- src/components/steps-form/StepsForm.jsx
+++ src/components/steps-form/StepsForm.jsx
@@ -10,6 +10,7 @@ import { validate } from "./utils.js"
 // components
 import FormHeader from "./components/FormHeader.jsx"
 import Button from "./components/Button.jsx"
+import Confirmation from "./components/Confirmation.jsx"
 
 export default function StepsForm() {
   // local state
@@ -69,9 +70,10 @@ export default function StepsForm() {
   return(
     <div className="desktop:flex-1">
       <form onSubmit={handleSubmit} className="flex flex-col min-h-[485px] flex-1 desktop:max-w-2xl desktop:h-full desktop:mx-auto desktop:min-h-full">
-        <section className="relative space-y-10 p-5 m-4 -mt-[105px] bg-white rounded-lg shadow-lg desktop:mx-0 desktop:-mt-0 desktop:shadow-none">
+        <section className="bg-red-200 relative p-5 m-4 -mt-[105px] bg-white rounded-lg shadow-lg overflow-hidden desktop:flex-1 desktop:mx-0 desktop:-mt-0 desktop:shadow-none">
           <FormHeader step={step} />
           <step.Component step={step} dispatch={appDispatch} monthly={steps[1].monthly} />
+          <Confirmation />
         </section>
         <footer className="flex justify-between mt-auto bg-white p-4 rounded-lg desktop:bg-transparent desktop:p-0 desktop:mt-none">
           <Button
