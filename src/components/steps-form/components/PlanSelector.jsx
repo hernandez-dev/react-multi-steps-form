1b00e81a7f06b3b825e2f105da89316a0b7f8167
Hernandez Maikol <hernandezdev@proton.me>
2024-04-03 11:23:36 -0400

    Components - steps-form/components - applying design margins and paddings to PlanSelector

--- src/components/steps-form/components/PlanSelector.jsx
+++ src/components/steps-form/components/PlanSelector.jsx
@@ -1,19 +1,19 @@
 export default function PlanSelector({ step, dispatch }) {
   return(
-    <div className="space-y-4">
-      <div className="grid gap-5 desktop:grid-cols-3">
+    <div className="space-y-8">
+      <div className="grid gap-[18px] desktop:grid-cols-3">
         {step.modalities.map(modality => {
           return(
             <button
               key={modality.id}
               type="button"
-              className={`outline-none flex w-full p-4 border ${modality.selected ? 'bg-alabaster border-purplish-blue' : 'border-gray-300'} rounded-lg text-left transition duration-300 desktop:flex-col desktop:justify-between desktop:h-[240px]`}
+              className={`outline-none flex w-full p-4 border ${modality.selected ? 'bg-alabaster border-purplish-blue' : 'border-gray-300'} rounded-xl text-left transition duration-300 desktop:flex-col desktop:justify-between`}
               onClick={() => dispatch({ type: "select-plan", step: 1, id: modality.id })}
             >
-              <span className="block w-12">
+              <span className="block w-[40px]">
                 <img src={`/images/${modality.icon}`} alt={`${modality.name} icon`} className="block w-full" />
               </span>
-              <div className="pl-4 space-y-3 desktop:pl-0">
+              <div className="pl-4 space-y-3 desktop:pl-0 desktop:mt-[48px]">
                 <h4 className="font-bold text-lg capitalize text-marine-blue leading-none">
                   {modality.name}
                 </h4>
@@ -36,10 +36,10 @@ export default function PlanSelector({ step, dispatch }) {
         </span>
         <button
           type="button"
-          className={`flex ${step.monthly ? '' : 'justify-end'} w-12 p-1 bg-marine-blue rounded-full`}
+          className={`flex ${step.monthly ? '' : 'justify-end'} w-[38px] h-[20px] p-[4px] bg-marine-blue rounded-full`}
           onClick={() => dispatch({ type: "select-monthly-plan", step: 1, value: !step.monthly })}
         >
-          <span className="block w-4 h-4 rounded-full bg-white"></span>
+          <span className="block w-[12px] h-[12px] rounded-full bg-white"></span>
         </button>
         <span className="block text-marine-blue leading-none">
           yearly
