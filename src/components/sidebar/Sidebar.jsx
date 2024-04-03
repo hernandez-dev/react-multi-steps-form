6d56e9bbad18cfbb76c76eb611e10cfec455ece4
Hernandez Maikol <hernandezdev@proton.me>
2024-04-03 12:50:03 -0400

    Components - sidebar - changing hover container top padding

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,12 +13,12 @@ export default function Sidebar() {
         <source media="(min-width:1100px)" srcSet="/images/bg-sidebar-desktop.svg" />
         <img src="/images/bg-sidebar-mobile.svg" alt="sidebar background" className="block w-full" />
       </picture>
-      <div className="absolute top-0 left-0 z-10 flex items-start justify-center gap-5 w-full h-full p-[2rem] desktop:block desktop:space-y-[2rem]">
+      <div className="absolute top-0 left-0 z-10 flex items-start justify-center gap-5 w-full h-full p-8 pt-10 desktop:block desktop:space-y-8">
         {steps.map((step, index) => {
           const active = index === currentStep
           return(
             <div key={step.id} className="flex">
-              <span className={`flex items-center justify-center w-[33px] h-[33px] border ${active ? "bg-pastel-blue border-patel-blue text-marine-blue" : "border-white text-white"} font-bold rounded-full`}>
+              <span className={`flex items-center justify-center w-[33px] h-[33px] border ${active ? "bg-light-blue border-light-blue text-marine-blue" : "border-white text-white"} font-bold rounded-full`}>
                 {index + 1}
               </span>
               <div className="hidden flex-1 pl-5 text-white uppercase tracking-widest desktop:flex desktop:flex-col desktop:justify-between">
