111f0035346e42d059cf82b9d00d8f8def9049fe
Hernandez Maikol <hernandezdev@proton.me>
2024-04-03 13:09:03 -0400

    Components - steps-form/components - changing elements vertical padding at FormHeader

--- src/components/steps-form/components/FormHeader.jsx
+++ src/components/steps-form/components/FormHeader.jsx
@@ -1,6 +1,6 @@
 export default function FormHeader({ step }) {
   return(
-    <header className="space-y-[22px] desktop:py-[47px]">
+    <header className="space-y-[22px] desktop:py-[45px]">
       <h1 className="font-bold text-3xl text-marine-blue leading-none">
         {step.heading.charAt(0).toUpperCase() + step.heading.slice(1)}
       </h1>
